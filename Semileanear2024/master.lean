f4c6cb7779477b322d3f4ed39bb1ddb38d9f66f1
Paul <paul.schwahn@universite-paris-saclay.fr>
2024-08-25 20:01:12 +0200

    Set up project

---
+++ Semileanear2024/master.lean
@@ -0,0 +1,16 @@
+import Semileanear2024.testproject.letsgo
+
+/-!
+# Semileanear 2024
+
+This is the master file where all the finished and unfinished projects may be showcased.
+
+Please create a subfolder for your project, just like "testproject" which is already there.
+
+In order to have a better structure, you may use imports.
+
+Have fun!
+-/
+
+--this theorem from Semileanear2024/testproject/letsgo.lean is now available:
+#check fundamental_theorem_of_algebra
---
+++ Semileanear2024/testproject/letsgo.lean
@@ -0,0 +1,8 @@
+--You probably want to import Mathlib.
+import Mathlib
+
+variable (f : Polynomial ℂ)
+variable (z : ℂ)
+
+theorem fundamental_theorem_of_algebra (hf : f.degree > 0) : ∃ z : ℂ, f.eval z = 0 :=
+  Complex.exists_root hf
