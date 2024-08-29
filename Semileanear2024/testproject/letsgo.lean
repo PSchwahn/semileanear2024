61944dde33c2ff7bbd6e28612a69e528f5392c8d
Zwenja <st169524@stud.uni-stuttgart.de>
2024-08-29 14:34:30 +0200

    Kommentar für die Maus

--- Semileanear2024/testproject/letsgo.lean
+++ Semileanear2024/testproject/letsgo.lean
@@ -4,5 +4,9 @@ import Mathlib
 variable (f : Polynomial ℂ)
 variable (z : ℂ)
 
+#check f
+
 theorem fundamental_theorem_of_algebra (hf : f.degree > 0) : ∃ z : ℂ, f.eval z = 0 :=
   Complex.exists_root hf
+
+  --ich lieb die Maus
