4aff7cfb36ff09767c23ed27bee06b1536bf803e
AliderWeise <Ali_der_Weise1234>
2024-08-29 06:40:59 +0200

    Hallo Paul

--- Semileanear2024.lean
+++ Semileanear2024.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Semileanear2024` library.
 -- Import modules here that should be built as part of the library.
 import «Semileanear2024».master
+
+-- hallo Leute
