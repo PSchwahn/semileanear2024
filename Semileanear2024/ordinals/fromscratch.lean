f293e91dd23b14148b464eb2b12745f278e7fbc7
Paul <paul.schwahn@universite-paris-saclay.fr>
2024-09-11 17:46:19 +0200

    update ordinals

--- Semileanear2024/ordinals/fromscratch.lean
+++ Semileanear2024/ordinals/fromscratch.lean
@@ -24,10 +24,37 @@ POSSIBLE DEFINITIONS:
  the relation <, so we kind of have a circle argument.
 -/
 
+namespace Semileanear
+
+variable (α : Type)
+
 --lean version of Nat is just an inductive type:
 inductive ourNat where
   | zero : ourNat
   | succ (o : ourNat) : ourNat
 -- limit ordinal: hard to define. need < relation and ordinal-indexed sequence
+-- inductive types, induction principle/tactic for Nat, then transfinite induction?
 
 #check IsWellOrder
+-- a well order is a TOTAL ORDER which is WELL-FOUNDED, i.e. every nonempty subset
+-- has a least element.
+-- A value is accessible if for all `y` such that `r y x`, `y` is also accessible.
+-- r is well-founded iff any element is accessible.
+--define relations, orders, well-orders
+--some examples for well orders: Fin n, Nat, omega+1
+#check wellOrderSucc
+#check IsWellOrderLimitElement
+-- limit element: neither a successor nor smallest element
+
+#check Ordinal
+-- quotient type of well-orderings on Type by order isomorphism
+
+--ordinal addition
+--limit ordinals
+--transfinite induction?
+#check Ordinal.addMonoidWithOne
+#check Ordinal.partialOrder
+#check Ordinal.induction
+--use induction tactic with ordinals?
+
+end Semileanear
