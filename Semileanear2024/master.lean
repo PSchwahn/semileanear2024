6eb6d498ce281c2732e8b234bf1cb9832bcc6f7a
Paul <paul.schwahn@universite-paris-saclay.fr>
2024-09-12 14:49:50 +0200

    some preliminaries from Amo's talk

--- Semileanear2024/master.lean
+++ Semileanear2024/master.lean
@@ -14,3 +14,9 @@ Have fun!
 
 --this theorem from Semileanear2024/testproject/letsgo.lean is now available:
 #check fundamental_theorem_of_algebra
+
+variable (P Q : Prop) (α : Type)
+#check P → Q -- Luckily, arrow types between propositions are themselves propositions
+#check ¬P -- in particular the negation, which is defined as ¬P := P → False
+#check List α -- List is a dependent type
+#check Type → Type -- here we go into a higher universe
