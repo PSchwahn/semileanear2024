9eadfaeea4838969b9872502d63212be40841e4b
AliderWeise <Ali_der_Weise1234>
2024-09-13 19:08:32 +0200

    last fix

--- Semileanear2024/sylow/basic.lean
+++ Semileanear2024/sylow/basic.lean
@@ -7,10 +7,6 @@ import Mathlib
 
 namespace Semileanear
 
-class ToType {A : Type u} (f : A→ Prop) where
-  a : A
-  h : f a
-
 section sectionBasic
 
 class LAction (α : Type u) (β : Type v) (γ : outParam (Type w)) where
@@ -28,7 +24,7 @@ class Inv (α : Type u) where
 class Mul (M : Type u) extends LAction M M M
 
 
-infixl:65 " * " => LAction.mul
+infixl:65 " ~ " => LAction.mul
 postfix:max "⁻¹" => Inv.inv
 
 
@@ -46,15 +42,15 @@ class One (α : Type u) where
 
 
 /-- Typeclass for expressing that a type `M` with multiplication and a one satisfies
-`1 * a = a` and `a * 1 = a` for all `a : M`. -/
+`1 ~ a = a` and `a ~ 1 = a` for all `a : M`. -/
 class MulOneClass (M : Type u) extends One M, Mul M where
-  one_mul : ∀ a : M, one * a = a
-  mul_one : ∀ a : M, a * one = a
+  one_mul : ∀ a : M, one ~ a = a
+  mul_one : ∀ a : M, a ~ one = a
 
 
-/-- A semigroup is a type with an associative `(*)`. -/
+/-- A semigroup is a type with an associative `(~)`. -/
 class Semigroup (G : Type u) extends Mul G where
-  mul_assoc : ∀ a b c : G, (a * b) * c = a * (b * c)
+  mul_assoc : ∀ a b c : G, (a ~ b) ~ c = a ~ (b ~ c)
 
 
 
@@ -63,8 +59,8 @@ class Monoid (M : Type u) extends Semigroup M, MulOneClass M
 
 
 class Group (G : Type u) extends Monoid G, Inv G where
-  inv_mul_cancel : ∀ a : G, a⁻¹ * a = one
-  mul_inv_cancel : ∀ a : G, a * a⁻¹ = one
+  inv_mul_cancel : ∀ a : G, a⁻¹ ~ a = one
+  mul_inv_cancel : ∀ a : G, a ~ a⁻¹ = one
 
 
 
@@ -97,10 +93,10 @@ instance instMonoid : Monoid ℕ where
 
 
 
-/-- `MulMemClass S M` says `S` is a type of sets `s : Set M` that are closed under `(*)` -/
+/-- `MulMemClass S M` says `S` is a type of sets `s : Set M` that are closed under `(~)` -/
 class MulMemClass (S : Type*) (M : Type*) [Mul M] [SetLike S M] : Prop where
   /-- A substructure satisfying `MulMemClass` is closed under multiplication. -/
-  mul_mem : ∀ {s : S} {a b : M}, a ∈ s → b ∈ s → a * b ∈ s
+  mul_mem : ∀ {s : S} {a b : M}, a ∈ s → b ∈ s → a ~ b ∈ s
 
 /-- `OneMemClass S M` says `S` is a type of subsets `s ≤ M`, such that `1 ∈ s` for all `s`. -/
 class OneMemClass (S : Type*) (M : Type*) [One M] [SetLike S M] : Prop where
@@ -109,7 +105,7 @@ class OneMemClass (S : Type*) (M : Type*) [One M] [SetLike S M] : Prop where
 
 
 /-- `SubmonoidClass S M` says `S` is a type of subsets `s ≤ M` that contain `1`
-and are closed under `(*)` -/
+and are closed under `(~)` -/
 class SubmonoidClass (S : Type*) (M : Type*) [MulOneClass M] [SetLike S M] extends MulMemClass S M, OneMemClass S M : Prop
 
 /-- `InvMemClass S G` states `S` is a type of subsets `s ⊆ G` closed under inverses. -/
@@ -129,7 +125,7 @@ structure Subsemigroup (M : Type u) [Mul M] where
   /-- The carrier of a subsemigroup. -/
   carrier : Set M
   /-- The product of two elements of a subsemigroup belongs to the subsemigroup. -/
-  mul_mem' {a b} : a ∈ carrier → b ∈ carrier → a * b ∈ carrier
+  mul_mem' {a b} : a ∈ carrier → b ∈ carrier → a ~ b ∈ carrier
 
 
 /-- A submonoid of a monoid `M` is a subset containing 1 and closed under multiplication. -/
@@ -160,7 +156,7 @@ instance [Monoid M] : SetLike (Submonoid M) M where
 
 
 instance [Monoid M] : SubmonoidClass (Submonoid M) M where
-  one_mem := Submonoid.one_mem
+  one_mem (s) := s.one_mem'
   mul_mem {s} := s.mul_mem'
 
 
@@ -188,7 +184,7 @@ section sectionProduct
 
 variable (G : Type u) [Group G]
 
-def ProductOfSubgroups (H K : Subgroup G) := {g : G | ∃ h k, h ∈ H.carrier ∧ k ∈ K.carrier ∧ g = h * k}
+def ProductOfSubgroups (H K : Subgroup G) := {g : G | ∃ h k, h ∈ H.carrier ∧ k ∈ K.carrier ∧ g = h ~ k}
 
 
 end sectionProduct
@@ -205,22 +201,25 @@ section sectionAction
 variable (G : Type u) [Group G] (S : Type u)
 
 class GroupAction extends LAction G S S where
-  one_id : ∀ s : S, (one : G) * s = s
-  assoc : ∀ (g h : G) (s : S), (g * h) * s = g * (h * s)
+  one_id : ∀ s : S, (one : G) ~ s = s
+  assoc : ∀ (g h : G) (s : S), (g ~ h) ~ s = g ~ (h ~ s)
 
 
 
 
+
+--def orbit {S G : Type u} [Group G] (s : S) (act : GroupAction G S)  :=
+--  Set.range fun g : G => act.mul g s
 
 def orbit {S G : Type u} [Group G] (s : S) (act : GroupAction G S)  :=
-  Set.range fun g : G => act.mul g s
+  {x : S // ∃ (g : G), act.mul g s = x}
 
 
 def stabilizer {S G : Type u} [Group G] (s : S) (act : GroupAction G S) : Subgroup G where
   carrier := { m | act.mul m s = s }
   one_mem' := act.one_id s
   mul_mem' {m m'} (ha : act.mul m s = s) (hb : act.mul m' s = s) :=
-    show act.mul (m * m') s = s by rw [act.assoc, hb, ha]
+    show act.mul (m ~ m') s = s by rw [act.assoc, hb, ha]
   inv_mem' {m} (h : act.mul m s = s) :=
     show act.mul m⁻¹ s = s by sorry
 
@@ -264,7 +263,7 @@ class Sylow extends Subgroup G where
   order : ∃ r : ℕ, Nat.card carrier = p ^ r ∧ isHighestPowerOfP (Nat.card G) p r
 
 
-def alpha_g (g : G) (h : G) : G := (g * h) * g⁻¹
+def alpha_g (g : G) (h : G) : G := (g ~ h) ~ g⁻¹
 
 instance alpha : GroupAction G (Sylow p G) := by sorry
   -- mul := fun (g : G) (S : Sylow p G) => {y : G | ∃ s, s ∈ S.carrier ∧ (alpha_g g s) = y }
@@ -284,14 +283,11 @@ variable (U : Subgroup G) [Group U] (isp : IsPGroup p U)
 
 variable (Q : Sylow p G)
 
-instance beta : GroupAction U (ToType (orbit Q (alpha p G))) := by sorry
+instance beta : GroupAction U (orbit Q (alpha p G)) := by sorry
 
-theorem card_eq_one_or_p_divides (Q P : Sylow p G) (h : P ∈ (orbit Q (alpha p G))):
-    (1 =  Nat.card (orbit ⟨P, h⟩ (beta p G U Q)))
-  ∨ (p ∣  Nat.card (orbit ⟨P, h⟩ (beta p G U Q))) := by sorry
-
-variable (Q P : Sylow p G) (h : P ∈ (orbit Q (alpha p G)))
-#check (beta p G U)
+theorem card_eq_one_or_p_divides (Q : Sylow p G) (P :  orbit Q (alpha p G)):
+    (1 =  Nat.card (orbit P (beta p G U Q)))
+  ∨ (p ∣  Nat.card (orbit P (beta p G U Q))) := by sorry
 
 --Satz a)
 theorem sylow_nonempty : Nonempty (Sylow p G) := by sorry
