15bb6d1a871f5748ee99969e1be5156af234d8ad
astrocbxy <chris@cbxy.de>
2024-09-14 15:42:13 +0200

    feat: add paperproof

--- lakefile.lean
+++ lakefile.lean
@@ -11,6 +11,9 @@ package «semileanear2024» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require Paperproof from git
+  "https://github.com/Paper-Proof/paperproof.git"@"main"/"lean"
+
 @[default_target]
 lean_lib «Semileanear2024» where
   -- add any library configuration options here
