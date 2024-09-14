3b03eb1eba4e1fad575a23d16e8b964072942f4e
Michael Eisermann <Michael.Eisermann@mathematik.uni-stuttgart.de>
2024-09-14 13:23:23 +0200

    Baustelle

--- Semileanear2024/sylow/action.lean
+++ Semileanear2024/sylow/action.lean
@@ -8,7 +8,7 @@ section sectionAction
 
 
 
-variable (G : Type u) [Group G] (S : Type u)
+variable (G : Type u) [group : Group G] (S : Type u)
 
 class GroupAction extends LAction G S S where
   one_id : ∀ s : S, (one : G) ~ s = s
@@ -21,8 +21,8 @@ class GroupAction extends LAction G S S where
 --def orbit {S G : Type u} [Group G] (s : S) (act : GroupAction G S)  :=
 --  Set.range fun g : G => act.mul g s
 
-def orbit {S G : Type u} [Group G] (s : S) (act : GroupAction G S)  :=
-  {x : S // ∃ (g : G), act.mul g s = x}
+def orbit {S G : Type u} [Group G] (x : S) (act : GroupAction G S)  :=
+  { y : S // ∃ (g : G), act.mul g x = y }
 
 
 def stabilizer {S G : Type u} [Group G] (s : S) (act : GroupAction G S) : Subgroup G where
@@ -31,7 +31,7 @@ def stabilizer {S G : Type u} [Group G] (s : S) (act : GroupAction G S) : Subgro
   mul_mem' {m m'} (ha : act.mul m s = s) (hb : act.mul m' s = s) :=
     show act.mul (m ~ m') s = s by rw [act.assoc, hb, ha]
   inv_mem' {m} (h : act.mul m s = s) :=
-    show act.mul m⁻¹ s = s by sorry
+    show act.mul m⁻¹ s = s by nth_rewrite 1 [← h, ← act.assoc, act.one_id ]; rfl
 
 
 instance (S: Type u) (F : Finset S) (f : F) (act : GroupAction G F): Finset (orbit f act) := by sorry
@@ -39,11 +39,30 @@ instance (S: Type u) (F : Finset S) (f : F) (act : GroupAction G F): Finset (orb
 
 def isTransitive (act : GroupAction G S) : Prop := ∀ (s1 s2 : S), ∃ (g : G), act.mul g s1 = s2
 
+open Function
 
-theorem orbit_theorem (act : GroupAction G S) : ∀ (x : S), Nat.card G = Nat.mul (Nat.card (orbit x act)) (Nat.card (stabilizer x act)) := by sorry
+theorem orbit_bijection (act : GroupAction G S) (x : S) :
+  ∃ (f : Prod (stabilizer x act) (orbit x act) → G), Bijective f := by
+    let t := fun ( y : orbit x act ) ↦ y.2.choose -- group element g with g x = y
+    let f := fun ( p : Prod (stabilizer x act) (orbit x act) ) ↦ group.mul p.1 (t p.2)
+    use f
 
+    -- have h1 : ∀ a : G, ∃ y : orbit x act, y.1 = act.mul a x ∧ y.2 = ⟨ a, rfl ⟩
 
-theorem lagrange (G : Type u) [Group G] (H : Subgroup G) : Nat.card H ∣ Nat.card G := by sorry
+    -- let g0 := fun ( a : G ) ↦ act.mul a x -- , ⟨ a, rfl ⟩ ⟩
+
+    -- let g := fun ( a : G ) ↦ ⟨ group.mul ( t ⟨ act.mul a x, ⟨ a, rfl ⟩ ⟩ ) a, act.mul a x ⟩
+
+    have inj : Injective f := by sorry
+    have sur : Surjective f := by sorry
+    exact ⟨ inj, sur ⟩
+
+theorem orbit_formula (act : GroupAction G S) : ∀ (x : S),
+  Nat.card G = Nat.card (stabilizer x act) * Nat.card (orbit x act) := by
+  intro x
+
+theorem lagrange (G : Type u) [Group G] (H : Subgroup G) :
+  Nat.card H ∣ Nat.card G := by sorry
 
 
 end sectionAction
